57aa602ef3ea8b29301afebc59b2a8e150b3f154
rabbitism <popmessiah@126.com>
2018-04-18 22:04:37 +0800

    Update according to project vue-manage-system

---
+++ AutoShelfWeb/src/page/login.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-title">AutoShelf Management System</div>
+        <div class="ms-login">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
+                <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" placeholder="username"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">Login</el-button>
+                </div>
+                <p style="font-size:12px;line-height:30px;color:#999;">Tips: Please contact administrator for authentication</p>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                ruleForm: {
+                    username: 'test1',
+                    password: 'pass'
+                },
+                rules: {
+                    username: [
+                        { required: true, message: 'Please enter user name', trigger: 'blur' }
+                    ],
+                    password: [
+                        { required: true, message: 'Please enter password', trigger: 'blur' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        localStorage.setItem('ms_username',this.ruleForm.username);
+                        this.$router.push('/readme');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .login-wrap{
+        position: relative;
+        width:100%;
+        height:100%;
+    }
+    .ms-title{
+        position: absolute;
+        top:50%;
+        width:100%;
+        margin-top: 100px;
+        text-align: center;
+        font-size:30px;
+        color: #409EFF;
+    }
+    .ms-login{
+        position: absolute;
+        left:50%;
+        top:50%;
+        width:300px;
+        height:160px;
+        margin:150px 0 0 -190px;
+        padding:40px;
+        border-radius: 5px;
+        background: #fff;
+    }
+    .login-btn{
+        text-align: center;
+    }
+    .login-btn button{
+        width:100%;
+        height:36px;
+    }
+</style>
